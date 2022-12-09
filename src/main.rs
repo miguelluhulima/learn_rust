e46262b50f4d1b9cbf7464607b6fdcd03d9ae4f9
miguelluhulima <miguelluhu5@gmail.com>
2022-12-09 16:32:24 +0700

    Updating a vector

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,10 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
-
 fn main() {
-    println!("Guess the number!");
+    let mut v: Vec<i32> = Vec::new();
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
 
-    println!("The secret number is: {secret_number}");
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!")
-    }
+    println!("{:?}", v);
 }
